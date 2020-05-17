adf5449e71f661566d0d5806340fe190ce604dcc
ssuzukin <suzukin0318@gmail.com>
2020-05-17 12:19:48 +0900

    add show_last_message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,17 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.text?
+        last_message.text
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだ投稿されていません'
+    end
+  end
+
 end
